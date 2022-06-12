46f084a4d8d222c9bcb05027fdb414ee8fe20195
Николай <rukin.nik.1996@mail.ru>
2022-06-13 01:23:38 +0700

    the second module

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
+function getRandom(min, max) {
+  if (max < min  || min < 0 || max <= 0) {
+    return ('Задан неверный диапозон');
+  }
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+
 function getRandomCoordinates(from, before, afterComma) {
   if (before < from  || from < 0 || before <= 0) {
     return ('Задан неверный диапозон');
   }
-
   const rounding = Math.random() * (before - from) + from;
   return rounding.toFixed(afterComma);
 }
-
-getRandomCoordinates();
