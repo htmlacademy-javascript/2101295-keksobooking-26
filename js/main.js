aabd63894359b041e82f431429caf2874bfe75ba
Николай <rukin.nik.1996@mail.ru>
2022-06-12 23:04:27 +0700

    second task

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-
 function getRandomCoordinates(from, before, afterComma) {
   if (before > from  || from < 0 || before <= 0) {
     return ('Задан неверный диапозон');
   }
 
-  return Math.random() * (befor - from) + from;
-
+  const rounding = Math.random() * (before - from) + from;
+  return rounding.toFixed(afterComma);
 }
+
+getRandomCoordinates();
