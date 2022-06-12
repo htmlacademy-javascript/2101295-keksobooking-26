f766cd98553da2b8d3b95a3c2ac3ec472bab5e0c
Николай <rukin.nik.1996@mail.ru>
2022-06-12 22:23:32 +0700

    module2-task1

---
+++ js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+function getRandomCoordinates(from, before, afterComma) {
+  if (before > from  || from < 0 || before <= 0) {
+    return ("Задан неверный диапозон")
+  }
+
+  return Math.random() * (befor - from) + mfrom;
+
+}
