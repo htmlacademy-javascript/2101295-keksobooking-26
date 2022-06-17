d35f8a5d08e90db6cbc2bc24551720df44e02eb6
Николай <rukin.nik.1996@mail.ru>
2022-06-18 00:39:10 +0700

    Исправил замечания

--- js/main.js
+++ js/main.js
@@ -13,35 +13,35 @@ function getRandomPositiveFloat (a, b, digits = 1) {
 }
 
 //нужен массив для генерации заголовка title
-const TITLEARR = ['отель Медвежий угол', 'отель Altay', 'гостиница пять звезд', 'Отель Марьин Остров'];
+const titles = ['отель Медвежий угол', 'отель Altay', 'гостиница пять звезд', 'Отель Марьин Остров'];
 
 //нужен массив для генерации typs
-const TYPSARR = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const types = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 
 //нужен массив для генерации checkins
-const CHECKINSARR = ['12:00', '13:00', '14:00'];
+const checkins = ['12:00', '13:00', '14:00'];
 
 //нужен массив для генерации checkout
-const CHECKOUTSARR = ['12:00', '13:00', '14:00'];
+const checkouts = ['12:00', '13:00', '14:00'];
 
 //нужен массив для генерации features
-const FEATURESARR = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const preparedFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 
 //нужен массив для генерации discription
-const DISCRIPTIONARR = [' База отдыха имеет три отдельно стоящих корпуса. Номерной фонд состоит из 14 номеров: три 2х-местных, один 3-х местный, десять 4х-местных.',
+const discriptions = [' База отдыха имеет три отдельно стоящих корпуса. Номерной фонд состоит из 14 номеров: три 2х-местных, один 3-х местный, десять 4х-местных.',
   'Гостиница отлично подходит для туристов, командированных, путешественников. К услугам гостей и жителям Барнаула - 14 уютных номеров.', 'Гостиница подойдет для краткосрочного пребывания туристов и командированных.', 'Гостиница расположена в жилом районе, в 4 км от центральной площади Советов. '];
 
 //нужен массив для генерации photos
-const PHOTOARR = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+const preparedPhotos = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
 
 // массив случайной длины из значений: wifi, dishwasher, parking, washer, elevator
-function getArrayFeatures(featuresArr) {
-  const lengthOfArray = getRandomPositiveInteger(1, featuresArr.length);
+function getArrayFeatures() {
+  const lengthOfArray = getRandomPositiveInteger(1, preparedFeatures.length);
   const arrayFe = [];
 
   while (arrayFe.length < lengthOfArray) {
-    const indexOfEl = getRandomPositiveInteger(0, featuresArr.length - 1);
-    const el = featuresArr[indexOfEl];
+    const indexOfEl = getRandomPositiveInteger(0, preparedFeatures.length - 1);
+    const el = preparedFeatures[indexOfEl];
 
     if (!arrayFe.includes(el)) {
       arrayFe.push(el);
@@ -51,49 +51,49 @@ function getArrayFeatures(featuresArr) {
 }
 
 //создаем массив случайной длины из значений photoArr
-const getArrayPhoto = (photoArr) => {
-  const getRandomLength = getRandomPositiveInteger(1, photoArr.length);
-  const resultArrPhoto = [];
+const getArrayPhoto = () => {
+  const getRandomLength = getRandomPositiveInteger(1, preparedPhotos.length);
+  const createdPhotos = [];
   for (let i = 0; i < getRandomLength; i++) {
-    const getIndEl = getRandomPositiveInteger(0, photoArr.length - 1);
-    resultArrPhoto.push(photoArr[getIndEl]);
+    const getIndEl = getRandomPositiveInteger(0, preparedPhotos.length - 1);
+    createdPhotos.push(preparedPhotos[getIndEl]);
   }
-  return resultArrPhoto;
+  return createdPhotos;
 };
 
 
 //переменная для фиксировангия колличества необходимых объектов
-const CREATEADVERTISEMENTSARR = 10;
+const NUMBER_OF_ADS = 10;
 
 //создаем функциб для создания объекта
 const createAdvertisements = () => ({
 
-  AUTHOR: {
+  author: {
     avatar: `img/avatars/user${  getRandomPositiveInteger(1, 10)  }.png`
   },
 
 
-  OFFER: {
-    title: TITLEARR[getRandomPositiveInteger(0, TITLEARR.length - 1)],
+  offer: {
+    title: titles[getRandomPositiveInteger(0, titles.length - 1)],
     address: `${getRandomPositiveFloat(0, 10, 4)  } , ${  getRandomPositiveFloat(0, 10, 4)}`,
     price:  getRandomPositiveInteger(1000, 10000),
-    type: TYPSARR[getRandomPositiveInteger(0, TYPSARR.length - 1)],
+    type: types[getRandomPositiveInteger(0, types.length - 1)],
     rooms: getRandomPositiveInteger(1, 5),
     guests: getRandomPositiveInteger(1, 12),
-    checkin: CHECKINSARR[getRandomPositiveInteger(0, CHECKINSARR.length - 1)],
-    checkout: CHECKOUTSARR[getRandomPositiveInteger(0, CHECKOUTSARR.length - 1)],
-    features: getArrayFeatures(FEATURESARR),
-    description: DISCRIPTIONARR[getRandomPositiveInteger(0, DISCRIPTIONARR.length - 1)],
-    photos: getArrayPhoto(PHOTOARR)
+    checkin: checkins[getRandomPositiveInteger(0, checkins.length - 1)],
+    checkout: checkouts[getRandomPositiveInteger(0, checkouts.length - 1)],
+    features: getArrayFeatures(preparedFeatures),
+    description: discriptions[getRandomPositiveInteger(0, discriptions.length - 1)],
+    photos: getArrayPhoto(preparedPhotos)
   },
 
-  LOCATION: {
+  location: {
     lat: getRandomPositiveFloat(35.65, 35.7, 5),
     lng: getRandomPositiveFloat(139.7, 139.8, 5)
 
   }
 });
 
-export const similarWizards = Array.from({length: CREATEADVERTISEMENTSARR}, createAdvertisements);
+export const similarWizards = Array.from({length: NUMBER_OF_ADS}, createAdvertisements);
 
 
