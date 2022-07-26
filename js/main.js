5fae992048fadd3396ac1943265c76123fc8b5ba
Николай <rukin.nik.1996@mail.ru>
2022-07-26 15:49:29 +0700

    Перламутровые пуговицы

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import './util.js';
+//import './filter.js';
 import './popup.js';
 import './form.js';
 import './user-form.js';
@@ -6,12 +7,91 @@ import './map.js';
 import './api.js';
 import {getData} from './api.js';
 import {showAlert} from './util.js';
-import {createMarker} from './map.js';
+import {createMarker, markerGroup} from './map.js';
+
+
+let offers = [];
+const defaultValue = 'any';
+const filterTypeElement = document.querySelector('#housing-type');
+const filterPriceElement = document.querySelector('#housing-price');
+const filterRoomElement = document.querySelector('#housing-rooms');
+const filterGuestsElement = document.querySelector('#housing-guests');
+
+const housingType = (i) => {
+  if (filterTypeElement.value === defaultValue) {return true;}
+  return i.offer.type === filterTypeElement.value;
+};
+
+const housingPrice = (i) => {
+  if (filterPriceElement.value === defaultValue) {return true;}
+  if (filterPriceElement.value === 'middle') {
+    return i.offer.price > 10000 && i.offer.price < 50000;
+  }
+  if (filterPriceElement.value === 'low') {
+    return i.offer.price < 10000;
+  }
+  if (filterPriceElement.value === 'high') {
+    return i.offer.price > 50000;
+  }
+};
+
+const housingRooms = (i) => {
+  if (filterRoomElement.value === defaultValue) {return true;}
+  return i.offer.rooms === Number(filterRoomElement.value);
+};
+
+const housingGuests = (i) => {
+  if (filterGuestsElement.value === defaultValue) {return true;}
+  return i.offer.guests === filterGuestsElement.value;
+};
+
+
+const housingFeatures = (i) => {
+  const checkboxes = document.getElementsByName('features');
+  const checkboxesChecked = [];
+
+  for (let v=0; v<checkboxes.length; v++) {
+    if (checkboxes[v].checked) {
+      checkboxesChecked.push(checkboxes[v].value);
+    }}
+
+  const listOfFeatures = i.offer.features;
+
+  if (checkboxesChecked.length === 0) {
+    return true;
+  }
+
+  if (!listOfFeatures) {
+    return true;}
+
+  for (let q = 0; q < checkboxesChecked.length; q++) {
+    if (!listOfFeatures.includes(checkboxesChecked[q])){
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const filters = document.querySelector('.map__filters');
+
+
+const filterOffers = (q) => housingGuests(q) && housingRooms(q) && housingPrice(q) && housingType(q) && housingFeatures(q);
+
+
+const updateMarkers = () => {
+  markerGroup.clearLayers();
+  createMarker(offers.filter(filterOffers).slice(0, 10));
+  filters.addEventListener('change',
+    updateMarkers
+  );
+};
 
 getData((ads) => {
-  ads.slice(0, 10).forEach((points) => {
-    createMarker(points);
-  });
-}, () => {
+  offers = ads;
+  updateMarkers();
+
+},
+() => {
   showAlert();
 });
